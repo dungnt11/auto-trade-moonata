a373b42688f1eca075f7380d7fbd609ae2420edd
Dung Nguyen <>
2022-02-27 11:17:38 +0700

    fix

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const CONFIG = {
 };
 
 puppeteer
-  .launch({ headless: false, args: ["--no-sandbox"] })
+  .launch({ headless: true, args: ["--no-sandbox"] })
   .then(async (browser) => {
     const page = await browser.newPage();
     await page.setViewport({ width: 1366, height: 768 });
